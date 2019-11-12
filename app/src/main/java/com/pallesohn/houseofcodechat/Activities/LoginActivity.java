e98325165f3e41d007a5eb63479e37dd3a8228a8
Palle Sohn Christensen <psohnchristensen@gmail.com>
2019-11-12 14:01:12 +0100

    add log out functionality

--- app/src/main/java/com/pallesohn/houseofcodechat/Activities/LoginActivity.java
+++ app/src/main/java/com/pallesohn/houseofcodechat/Activities/LoginActivity.java
@@ -2,12 +2,14 @@ package com.pallesohn.houseofcodechat.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.pallesohn.houseofcodechat.R;
 
@@ -16,14 +18,33 @@ public class LoginActivity extends AppCompatActivity {
     private Button facebookBtn, googleBtn;
     private FirebaseUser currentUser;
 
+    private static FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
         facebookBtn = findViewById(R.id.facebookBtn);
         googleBtn = findViewById(R.id.googleBtn);
+
+        facebookBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/pallesohn/houseofcodechat/Activities/MainActivity.java
+++ app/src/main/java/com/pallesohn/houseofcodechat/Activities/MainActivity.java
@@ -1,13 +1,17 @@
 package com.pallesohn.houseofcodechat.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.pallesohn.houseofcodechat.R;
 import com.pallesohn.houseofcodechat.TabsAccessorAdapter;
@@ -20,11 +24,15 @@ public class MainActivity extends AppCompatActivity {
     private TabsAccessorAdapter mTabsAccessorAdapter;
 
     private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
         mToolbar = findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
@@ -50,5 +58,32 @@ public class MainActivity extends AppCompatActivity {
     private void sendUserToLoginActivity() {
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.main_find_friends_option) {
+
+        }
+        if(item.getItemId() == R.id.main_settings_option) {
+
+        }
+        if(item.getItemId() == R.id.main_logout_option) {
+            mAuth.signOut();
+            sendUserToLoginActivity();
+        }
+
+        return true;
     }
 }
